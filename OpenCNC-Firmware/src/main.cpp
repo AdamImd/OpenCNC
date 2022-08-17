0069d1ca55b05f4214bc73442ce69fae6e89c851
Adam Imdieke <imdie022@umn.edu>
2022-08-17 15:28:56 -0600

    Updated testing pattern

--- OpenCNC-Firmware/src/main.cpp
+++ OpenCNC-Firmware/src/main.cpp
@@ -24,48 +24,33 @@ void setup() {
   digitalWrite(EN_y, LOW);
   digitalWrite(DIR_y, LOW);
   digitalWrite(STP_y, LOW);
+
+  delay(5000);
 }
 
 int dir=0;
 
-void loop() {
-  //---------
-//  const int NUM=800;
-//  const int DEL=20;
-//  digitalWrite(EN, LOW);
-//  digitalWrite(DIR,dir);
-//  for(int i=0; i<NUM; i++){
-//    digitalWrite(STP, HIGH);
-//    delayMicroseconds(DEL+i/100);
-//    digitalWrite(STP, LOW);
-//    delayMicroseconds(DEL+i/100);
-//  }
-//  digitalWrite(EN, HIGH);
-//  dir=!dir;
-
 
   digitalWrite(DIR_x,dir);
   digitalWrite(DIR_y,dir);
-  for(int i=200; i>15; i*=.99){
-    for(int j=0; j<50; j++){
-      digitalWrite(STP_x, HIGH);
-      digitalWrite(STP_y, HIGH);
-      delayMicroseconds(i);
-      digitalWrite(STP_x, LOW);
-      digitalWrite(STP_y, LOW);
-      delayMicroseconds(i);
-    }
+  int del = 100;
+  for(int j=0; j<5000; j++){
+    digitalWrite(STP_x, HIGH);
+    digitalWrite(STP_y, HIGH);
+    delayMicroseconds(del);
+    digitalWrite(STP_x, LOW);
+    digitalWrite(STP_y, LOW);
+    delayMicroseconds(del);
   }
-  for(int i=80; i<200; i+=1){
-    for(int j=0; j<50; j++){
-      digitalWrite(STP_x, HIGH);
-      digitalWrite(STP_y, HIGH);
-      delayMicroseconds(i);
-      digitalWrite(STP_x, LOW);
-      digitalWrite(STP_y, LOW);
-      delayMicroseconds(i);
-    }
+  for(int j=0; j<5000; j++){
+    digitalWrite(STP_x, HIGH);
+    digitalWrite(STP_y, HIGH);
+    delayMicroseconds(del);
+    digitalWrite(STP_x, LOW);
+    digitalWrite(STP_y, LOW);
+    delayMicroseconds(del);
   }
+
   dir=!dir;
   delay(1000);
 }
