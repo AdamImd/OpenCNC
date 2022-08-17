d2849878ebd2c5cf25d587baba3bc5c63199ace7
Adam Imdieke <imdie022@umn.edu>
2022-08-17 16:10:48 -0500

    Added Y stepper

--- OpenCNC-Firmware/src/main.cpp
+++ OpenCNC-Firmware/src/main.cpp
@@ -1,17 +1,29 @@
 #include <Arduino.h>
 
-const int EN=5;
-const int DIR=4;
-const int STP=16;
+const int EN_x=21;
+const int DIR_x=22;
+const int STP_x=23;
+
+const int EN_y=18;
+const int DIR_y=19;
+const int STP_y=20;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(EN, OUTPUT);
-  pinMode(DIR, OUTPUT);
-  pinMode(STP, OUTPUT);
-  digitalWrite(EN, LOW);
-  digitalWrite(DIR, LOW);
-  digitalWrite(STP, LOW);
+  pinMode(EN_x, OUTPUT);
+  pinMode(DIR_x, OUTPUT);
+  pinMode(STP_x, OUTPUT);
+  digitalWrite(EN_x, LOW);
+  digitalWrite(DIR_x, LOW);
+  digitalWrite(STP_x, LOW);
+  
+
+  pinMode(EN_y, OUTPUT);
+  pinMode(DIR_y, OUTPUT);
+  pinMode(STP_y, OUTPUT);
+  digitalWrite(EN_y, LOW);
+  digitalWrite(DIR_y, LOW);
+  digitalWrite(STP_y, LOW);
 }
 
 int dir=0;
@@ -32,20 +44,25 @@ void loop() {
 //  dir=!dir;
 
 
-  digitalWrite(DIR,dir);
+  digitalWrite(DIR_x,dir);
+  digitalWrite(DIR_y,dir);
   for(int i=200; i>15; i*=.99){
     for(int j=0; j<50; j++){
-      digitalWrite(STP, HIGH);
+      digitalWrite(STP_x, HIGH);
+      digitalWrite(STP_y, HIGH);
       delayMicroseconds(i);
-      digitalWrite(STP, LOW);
+      digitalWrite(STP_x, LOW);
+      digitalWrite(STP_y, LOW);
       delayMicroseconds(i);
     }
   }
   for(int i=80; i<200; i+=1){
     for(int j=0; j<50; j++){
-      digitalWrite(STP, HIGH);
+      digitalWrite(STP_x, HIGH);
+      digitalWrite(STP_y, HIGH);
       delayMicroseconds(i);
-      digitalWrite(STP, LOW);
+      digitalWrite(STP_x, LOW);
+      digitalWrite(STP_y, LOW);
       delayMicroseconds(i);
     }
   }
