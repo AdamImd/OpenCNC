4e94281758b0e3312ade46a5436050e21b6a50c6
Adam Imdieke <imdie022@umn.edu>
2022-08-19 20:28:27 -0500

    Updated LinearAxis

--- OpenCNC-Firmware/lib/LinearAxis/src/LinearAxis.cpp
+++ OpenCNC-Firmware/lib/LinearAxis/src/LinearAxis.cpp
@@ -1,8 +1,33 @@
 #include "LinearAxis.h"
+#include "LimitSwitch.h"
 #include "Arduino.h"
 #include <stdint.h>
 
-LinearAxis::LinearAxis(StepperDriver* axis) {
+LinearAxis::LinearAxis(StepperDriver* axis, LimitSwitch* sw) {
     driver = axis;
-    
+    limit = sw;
+    status = 0;
+    steps_mm = 1064;
+}
+
+uint8_t LinearAxis::zero() {
+    if (!driver->get_enabled()) return 0;
+    while (!limit->is_closed()){
+        driver->step(0);
+        delayMicroseconds(200);
+    }
+    driver->set_position(0);
+    return 1;
+}
+
+uint8_t LinearAxis::move_config(int32_t target_pos, float speed) {
+    if (!driver->get_enabled()) return 0;
+
+    target = target_pos;
+    float step_time = speed * steps_mm;
+    if(!timer.begin(*(this->callback), step_time))
+        return 0;
+
+    status = 1; 
+    return 1;
 }
--- OpenCNC-Firmware/lib/LinearAxis/src/LinearAxis.h
+++ OpenCNC-Firmware/lib/LinearAxis/src/LinearAxis.h
@@ -3,13 +3,23 @@
 
 #include <stdint.h>
 #include "StepperDriver.h"
+#include "LimitSwitch.h"
 
 class LinearAxis {
     private: 
         IntervalTimer timer;
         StepperDriver* driver;
+        LimitSwitch* limit;
+        uint8_t status;
+        int32_t target;
+        uint32_t steps_mm;
+        void callback();
     public:
-        LinearAxis(StepperDriver* axis);
+        LinearAxis(StepperDriver* axis, LimitSwitch* sw);
+        uint8_t zero();
+        uint8_t move_config(int32_t target, float speed);
+        uint8_t move_begin();
+        uint8_t move_complete();
 };
 
 #endif
